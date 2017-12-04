b3c0e5cdd902f98da061e85c27ab82a03983fdab
Michael Silberstein <mikesilb123@gmail.com>
2017-12-04 16:36:38 -0500

    Completed 16 mini-modules from AvetisG Programmining C# tutorial

---
+++ AventisG C# Tutorial/Method/Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.GetFullName("Mike", "Silberstein");
+        }
+    }
+
+    class Person
+    {
+        public void GetFullName(string FirstName, string LastName)
+        {
+            Console.WriteLine($"The fullname of the person is {FirstName} {LastName}.");
+        }
+    }
+}
