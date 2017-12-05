c45f4a03291565d4a2027f681a999d52a70d9835
Michael Silberstein <mikesilb123@gmail.com>
2017-12-05 18:22:53 -0500

    Completed 27 mini-modules from AvetisG Programmining C# tutorial

---
+++ AventisG C# Tutorial/Enum/Enum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            if (player.Weapon == Weapons.MachineGun)
+            {
+                Console.WriteLine("Run!");
+            }
+
+            else if (player.Weapon == Weapons.Uzi)
+            {
+                Console.WriteLine("Give Up");
+            }
+
+            else if (player.Weapon == Weapons.HandGun)
+            {
+                Console.WriteLine("Hide");
+            }
+        }
+    }
+    class Player
+    {
+            public Weapons Weapon { get; set;  }
+    }
+
+    public enum Weapons
+    {
+        HandGun,
+        MachineGun,
+        Uzi,
+        FlameThrower
+    }
+}
---
+++ AventisG C# Tutorial/StaticNonStatic/StaticNonStatic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StaticNonStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Mike", "Silb");
+            Console.WriteLine(person.FormatFullName());
+            Console.WriteLine(Person.FormatFullNameStatic("Michael", "Silberstein"));
+        }
+    }
+
+    class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public Person (string Firstname, string Lastname) 
+        {
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+        }
+
+        public static string FormatFullNameStatic(string Firstname, string Lastname)
+        {
+            return $"{Firstname} - {Lastname}";
+        }
+
+        public string FormatFullName()
+        {
+            return $"{Firstname} - {Lastname}";
+        }
+
+    }
+}
