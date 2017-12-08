251569ebf0b002c931fa5b29ed180b86e4d3fab6
Michael Silberstein <mikesilb123@gmail.com>
2017-12-07 20:45:37 -0500

    Completed 42 mini-modules from AvetisG Programmining C# tutorial

---
+++ AventisG C# Tutorial/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    abstract class Person
+    {
+        private readonly string _firstname;
+        private readonly string _lastname;
+
+        protected Person(string firstname, string lastname)
+        {
+            _firstname = firstname;
+            _lastname = lastname;
+        }
+
+        public abstract void Greet();
+
+        public string GetFullName()
+        {
+            return $"{_firstname} {_lastname}";
+        }
+    }
+}
