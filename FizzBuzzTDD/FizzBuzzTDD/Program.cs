9b1edb11922f5b75bcde349f1d235114a1955dfe
Michael Silberstein <mikesilb123@gmail.com>
2017-12-10 10:42:16 -0500

    Completed TDD tutorial based on FizzBuzz-based TDD

---
+++ FizzBuzzTDD/FizzBuzzTDD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FizzBuzz.Library;
+
+namespace FizzBuzzTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num < 101; num++)
+            {
+                Console.WriteLine(FizzBuzzer.GetValue(num));
+            }
+        }
+    }
+}
